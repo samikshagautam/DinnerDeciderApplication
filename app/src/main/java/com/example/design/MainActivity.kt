230955bc700d691d4f98e0a7f3aafe26fa5ea988
Samiksha Gautam <samikshyagautam5@gmail.com>
2019-11-29 20:43:07 -0600

    Initial commit

---
+++ app/src/main/java/com/example/design/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.design
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //add an ArrayList with default values { Hamburger, Pizza, Mexican, American, Chinese }
+    var foodList:List<String> = arrayListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese")
+
+    //add new food to ArrayList
+    fun addfood_click(view: View) {
+        foodList+=foodtext.text.toString()
+    }
+
+    //show random food when Decide is clicked
+    fun decide_click(view: View) {
+        var i:Int = Random.nextInt(0, foodList.size)
+        foodview.text = foodList.get(i)
+    }
+
+}
